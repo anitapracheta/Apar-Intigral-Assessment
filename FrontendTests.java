cc3a4069aba46380c7b00701499cc25203137bfc
anitapracheta <anita.pracheta@gmail.com>
2024-07-17 19:29:53 +0530

    Add files via upload

--- FrontendTests.java
+++ FrontendTests.java
@@ -1,19 +1,22 @@
 package com.mytestframework.frontend;
 
 import com.mytestframework.utils.BrowserUtils;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
-import java.util.List;
 
+import java.time.Duration;
+import java.util.List;
 
 public class FrontendTests {
 
@@ -33,78 +36,55 @@ public class FrontendTests {
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
-           // driver.quit();
+            driver.quit();
             extent.flush();
         }
     }
 
-    @Test
-    public void validateSubscriptionPackages() {
+    @DataProvider(name = "countriesData")
+    public Object[][] countriesData() {
+        return new Object[][]{
+            {"sa", "Saudi Arabia", "15", "25", "60", "SAR"},
+            {"kw", "Kuwait", "1.2", "2.5", "4.8", "KWD"},
+            {"bh", "Bahrain", "1.5", "3", "6", "BHD"}
+        };
+    }
+
+    @Test(dataProvider = "countriesData")
+    public void validateSubscriptionPackages(String code, String country, String litePrice, String classicPrice, String premiumPrice, String currency) {
         driver.get("https://subscribe.stctv.com/sa-en");
         driver.manage().window().maximize();
 
-        // Array of countries with their expected prices and currencies
-        String[][] countriesDetails = {
-            {"sa","Saudi Arabia", "15", "25", "60", "SAR"},
-            {"kw","Kuwait", "1.2", "2.5", "4.8", "KWD"},
-            {"bh","Bahrain", "1.5", "3", "6", "BHD"}
-        };
-        
-        
-       
-
-        for (String[] country : countriesDetails) {
-        	String code = country[0];
-            String countryName = country[1];
-            String litePrice = country[2];
-            String classicPrice = country[3];
-            String premiumPrice = country[4];
-            String currency = country[5];
-
-            validateCountryPackages(code,countryName, litePrice, classicPrice, premiumPrice, currency);
-        }
+        validateCountryPackages(code, country, litePrice, classicPrice, premiumPrice, currency);
     }
 
-    private void validateCountryPackages(String code,String country, String litePrice, String classicPrice, String premiumPrice, String currency) {
-        
-    	
-        WebElement countryButton = driver.findElement(By.cssSelector("#country-name"));
-        countryButton.click();
-        driver.findElement(By.id(code)).click();;
-        
-        
+    private void validateCountryPackages(String code, String country, String litePrice, String classicPrice, String premiumPrice, String currency) {
+    	 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.findElement(By.cssSelector("#country-name")).click();
+        driver.findElement(By.id(code)).click();
 
-        // Wait for the page to load the packages (you might need to use WebDriverWait for a better wait)
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-      
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("plan-title")));
+
         List<WebElement> packageNames = driver.findElements(By.className("plan-title"));
-        for (WebElement packageName:packageNames) {
-        	String type = packageName.getText().trim();
-        	String type_l=type.toLowerCase();
-        	List<WebElement> packageElements = driver.findElements(By.className("plan-name"));
-       
-        for (WebElement packageElement : packageElements) {
-            
-            String price = packageElement.findElement(By.cssSelector("div[id='currency-"+type_l+"'] b")).getText().trim();
-            
-            String Curr_text = packageElement.findElement(By.cssSelector("div[id='currency-"+type_l+"'] i")).getText();
-            String packageCurrency= Curr_text.substring(0, Curr_text.indexOf('/'));
+        for (WebElement packageName : packageNames) {
+            String type = packageName.getText().trim().toLowerCase();
+
+            WebElement packageElement = driver.findElement(By.cssSelector("div[id='currency-" + type + "']"));
+            String price = packageElement.findElement(By.tagName("b")).getText().trim();
+            String packageCurrency = packageElement.findElement(By.tagName("i")).getText().split("/")[0].trim();
+
             System.out.println("Country: " + country + ", Type: " + type + ", Price: " + price + ", Currency: " + packageCurrency);
 
             switch (type) {
-                case "LITE":
+                case "lite":
                     Assert.assertEquals(price, litePrice, "Lite package price mismatch in " + country);
                     Assert.assertEquals(packageCurrency, currency, "Lite package currency mismatch in " + country);
                     break;
-                case "Classic":
+                case "classic":
                     Assert.assertEquals(price, classicPrice, "Classic package price mismatch in " + country);
                     Assert.assertEquals(packageCurrency, currency, "Classic package currency mismatch in " + country);
                     break;
-                case "Premium":
+                case "premium":
                     Assert.assertEquals(price, premiumPrice, "Premium package price mismatch in " + country);
                     Assert.assertEquals(packageCurrency, currency, "Premium package currency mismatch in " + country);
                     break;
@@ -113,18 +93,4 @@ public class FrontendTests {
             }
         }
     }
-    
 }
-}
-        
-    
-
-
-
-
-
-
-
-
-
-
